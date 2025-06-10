9134c3c7b66b89c4ab5d79843ca093682d5489da
kirti-khot- <khotkirti2005@gmail.com>
2025-06-10 15:57:19 +0530

    start page set

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
@@ -24,4 +24,4 @@ const App = () => {
     
   )
 }
-export default App
+export default App;
