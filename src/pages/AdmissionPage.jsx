6a049415754aefbb3c46a6d7c3d5ae4f165fa278
kirti-khot- <khotkirti2005@gmail.com>
2025-06-10 20:32:24 +0530

    first commit

--- src/pages/AdmissionPage.jsx
+++ src/pages/AdmissionPage.jsx
@@ -11,8 +11,8 @@ const AdmissionPage = () => {
                 <p>Welcome to the Admissions page of Vivekanand College! We are delighted you're considering 
                 joining our vibrant academic community. Our admission process is designed to be straightforward 
                 and accessible.</p>
-                <p>Please review the eligibility criteria and key dates below for the upcoming academic year. 
-                For any queries, feel free to contact our admissions office.</p>
+                <p>Please review the eligibility criteria and key dates below for the upcoming academic year.For
+                 any queries, feel free to contact our admissions office.</p>
                 <h2>Eligibility Criteria for Undergraduate Programs</h2>
                 <table border="solid" className="admissiontable">
                     <tr>
@@ -43,7 +43,7 @@ const AdmissionPage = () => {
                 <h2>Application Process</h2>
                 <ol>
                     <li>Online Application: Fill out the application form available on our portal.</li>
-                    <li>Document Submission: Upload scanned copies of required documents (mark sheets, ID proof, etc.).</li>
+                    <li>Document Submission: Upload scanned copies of required documents (mark sheets, ID proof, etc.)</li>
                     <li>Entrance Exam (if applicable):Appear for the college's entrance examination.</li>
                     <li>Merit List & Interview: Check the merit list and attend the interview if shortlisted.</li>
                     <li>Fee Payment: Complete the admission by paying the required fees.</li>
